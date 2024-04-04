d96beddcf7645f5dc42e1e23bd5311daaf83baeb
Steve MacLeod <steve@causeworks.com>
2024-04-03 23:30:01 -0400

    Tmux and Shell aliases Added some shell aliases Added tmux plugin manager tpm Added vim-tmux-navigator to tmux and nvim

--- terminal.nix
+++ terminal.nix
@@ -5,6 +5,10 @@
 	programs.bash.enable = true;
 
 	home.shellAliases = {
+		# Might be cool to make a nix function to be able to break up
+		# args on separate lines
+		hm = "home-manager";
+		less = "less --tabs=4 --chop-long-lines";
 		xclip = "xclip -selection clipboard";
 	};
 
